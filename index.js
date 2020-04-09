64c9cc712a25aee422a3c3ac44919bac19c10a62
wii <rwii0433@cchntek.com>
2020-04-09 11:58:00 +0800

    test

--- index.js
+++ index.js
@@ -1,14 +1,9 @@
-const http = require("http");
+const SocketServer = require('ws').Server;
+const express = require('express');
 
 const hostname = "127.0.0.1";
 const port = process.env.PORT || 5000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-  res.end("Hello World\n");
-});
+const server = express().listen(port, () => console.log(`Listening on ${port}`))
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+console.log(server);    
