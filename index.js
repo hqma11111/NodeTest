2e329465eee188f26a5dddb6688df58d314f9ca5
wii <rwii0433@cchntek.com>
2020-04-09 13:53:58 +0800

    test

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 const SocketServer = require('ws').Server;
 const express = require('express');
 
-const hostname = "127.0.0.1";
 const port = process.env.PORT || 5000;
-
 const server = express().listen(port, () => console.log(`Listening on ${port}`))
 
-console.log(server);    
+const wss = new SocketServer({ server })
+
+//當 WebSocket 從外部連結時執行
+wss.on('connection', ws => {
+
+    //連結時執行此 console 提示
+    console.log('Client connected');
+
+    const sendNowTime = setInterval(()=>{
+        ws.send(String(new Date()));
+    },1000);
+
+    //當 WebSocket 的連線關閉時執行
+    ws.on('close', () => {
+        console.log('Close connected')
+    })
+    }
+);
